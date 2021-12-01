b3ab3c282ed36cf1bb313a5f54779cc6b6c05b3c
Hey Soo Kim (김혜수) <85905612+sookim482@users.noreply.github.com>
2021-12-01 10:02:12 +0900

    Update merge_two_sorted_lists.dart

--- merge_two_sorted_lists.dart
+++ merge_two_sorted_lists.dart
@@ -1,1 +1,13 @@
+// https://leetcode.com/problems/merge-two-sorted-lists/
+void main(){
+  
+  print(mergedList([1,2,4], [1,3,4])); // [1, 1, 2, 3, 4, 4]
+  print(mergedList([], [])); // []
+  print(mergedList([], [0])); // [0]
+}
 
+List<int>? mergedList(List<int> l1, List<int> l2){
+  l1.addAll(l2.map((int i) => i)); 
+  l1.sort();
+  return l1;
+}
