70af0b8c451b46f13e05b00b226a6c949d1bdfa8
Hey Soo Kim (김혜수) <85905612+sookim482@users.noreply.github.com>
2021-12-09 21:26:06 +0900

    Create add_binary.dart

---
+++ add_binary.dart
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/add-binary/
+
+void main(){
+  
+  binarySum("11", "1"); // "100"
+  binarySum("1010", "1011"); // "10101"
+}
+
+String? binarySum(String a, String b){
+  
+}
